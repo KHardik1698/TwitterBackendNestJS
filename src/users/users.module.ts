c106388de6c3d2fca2786f1d8a29ce1bffd65c85
KHardik1698 <hardik.kundalia@contentstack.com>
2021-03-03 21:13:32 +0530

    Created a Module for User
    The Module will use the Mongoose Module to connect to User Schema, User Controller & User Service

---
+++ src/users/users.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './schemas/users.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'TwitterUser',
+        schema: UserSchema,
+      },
+    ]),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
+export class UsersModule {}
