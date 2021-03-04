97beac457c44c3e87abde0bc73bd2a2fd0a60d8c
KHardik1698 <hardik.kundalia@contentstack.com>
2021-03-04 20:58:59 +0530

    Added Class Validators to UserDto Class

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,7 +1,17 @@
+import { IsNotEmpty, IsEmail, IsString } from 'class-validator';
+
 export class UserDto {
   id: string;
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+  @IsNotEmpty()
+  @IsString()
   username: string;
+  @IsNotEmpty()
+  @IsString()
   password: string;
+  @IsNotEmpty()
+  @IsString()
   confirmPassword: string;
 }
