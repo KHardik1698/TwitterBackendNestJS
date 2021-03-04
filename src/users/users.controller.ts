bec4ce19188d46b92e88ca96f3459111689cfdbb
KHardik1698 <hardik.kundalia@contentstack.com>
2021-03-04 12:46:14 +0530

    Modified Get & Post Controllers
    Added Express Response to send Http Status Code & Request Status in the Response along with the Data

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,39 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Res } from '@nestjs/common';
 import { UserDto } from './dto/user.dto';
 import { UsersService } from './users.service';
+import { Response } from 'express';
 
 @Controller('users')
 export class UsersController {
   constructor(private userService: UsersService) {}
 
   @Get()
-  public getUsers() {
-    return this.userService.getUsers();
+  public async getUsers(@Res() response: Response) {
+    let result = await this.userService.getUsers().then((response) => {
+      return response;
+    });
+    if (result) {
+      return response.status(200).json({
+        status: 'Successful',
+        data: result,
+      });
+    }
   }
   @Post()
-  public postUser(@Body() UserDto: UserDto) {
-    this.userService.postUser(UserDto);
+  public async postUser(
+    @Body() PostUserDto: UserDto,
+    @Res() response: Response,
+  ) {
+    let result = await this.userService
+      .postUser(PostUserDto)
+      .then((response) => {
+        return response;
+      });
+    if (result) {
+      return response.status(200).json({
+        status: 'Successful',
+        data: result,
+      });
+    }
   }
 }
