5601f2dfd7ecf9383877d13e518a4f5f66c826c7
KHardik1698 <hardik.kundalia@contentstack.com>
2021-03-04 20:58:22 +0530

    Added ValidationPipe globally for using Class Validations
    Enabled CORS in the App

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 const port = process.env.PORT;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
   Logger.log(`Server started running on http://localhost:${port}`, 'Bootstrap');
 }
