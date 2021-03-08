f52f251a718ebbba652ee181afa7a7cb6786c9da
KHardik1698 <hardik.kundalia@contentstack.com>
2021-03-08 14:52:55 +0530

    Added compression to Main App as a Global Middleware

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import * as compression from 'compression';
 
 const port = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(compression());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
   Logger.log(`Server started running on http://localhost:${port}`, 'Bootstrap');
