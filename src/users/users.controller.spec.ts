03d0814a98db5ce82c4fc5b33caf1401a50f83bc
KHardik1698 <hardik.kundalia@contentstack.com>
2021-03-08 20:27:44 +0530

    Added TwitterUser Model Provider to Users Services & Users Controller Test Component, which will use value as UserDto

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { UserDto } from './dto/user.dto';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -7,6 +10,13 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        UsersService,
+        {
+          provide: getModelToken('TwitterUser'),
+          useValue: UserDto,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { UserDto } from './dto/user.dto';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getModelToken('TwitterUser'),
+          useValue: UserDto,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -14,5 +22,9 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return array of all users', () => {
+    expect('/get').toReturn();
   });
 });
