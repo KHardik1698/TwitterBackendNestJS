d8b96495fec62e010b926d479c6ab35e24d12b3e
KHardik1698 <hardik.kundalia@contentstack.com>
2021-03-11 12:17:18 +0530

    Added AllExceptionsFilter to Main App so that it catches all Exceptions Globally

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import * as compression from 'compression';
+import { AllExceptionsFilter } from './exception.filter';
 
 const port = process.env.PORT;
 
@@ -9,6 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.use(compression());
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(port);
   Logger.log(`Server started running on http://localhost:${port}`, 'Bootstrap');
 }
