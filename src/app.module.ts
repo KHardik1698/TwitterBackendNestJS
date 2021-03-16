235c7efa0b6c0f71bcb801d97f5ee51ce0f63755
KHardik1698 <hardik.kundalia@contentstack.com>
2021-03-16 12:08:08 +0530

    Added AuthenticationModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.DATABASE_URL),
     UsersModule,
+    AuthenticationModule,
   ],
 })
 export class AppModule {}
