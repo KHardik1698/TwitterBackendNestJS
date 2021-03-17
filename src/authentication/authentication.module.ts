ceaea74caf787ad42b5c55978b9e7683dbbe961e
KHardik1698 <hardik.kundalia@contentstack.com>
2021-03-17 13:00:59 +0530

    Added ConfigModule & JwtModule to AuthenticationModule

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,11 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthenticationService } from './authentication.service';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    forwardRef(() => UsersModule),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.SECRET,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   providers: [AuthenticationService, LocalStrategy],
+  exports: [AuthenticationService, JwtModule],
 })
 export class AuthenticationModule {}
