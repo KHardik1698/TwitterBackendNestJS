d6a68f6d94f58513a0ec3cc407bcff5146465bd1
KHardik1698 <hardik.kundalia@contentstack.com>
2021-03-17 12:56:22 +0530

    Added JwtService to AuthenticationService
    Created a Login Function which returns an Access Token generated using Username & UserId

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { HttpExceptionClass } from '../users/helpers/users.httpexception';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthenticationService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(username: string, password: string): Promise<any> {
     const user = await this.usersService.getUserByUsername(username);
@@ -20,5 +24,12 @@ export class AuthenticationService {
       );
     }
     return user;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
