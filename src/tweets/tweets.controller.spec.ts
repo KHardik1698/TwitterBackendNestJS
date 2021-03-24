f00cfb9f1e1d484c7234666a67150a43ad681bd6
KHardik1698 <hardik.kundalia@contentstack.com>
2021-03-24 17:59:01 +0530

    Added TwitterService, TwitterTweetModel & TweetDto to Providers

--- src/tweets/tweets.controller.spec.ts
+++ src/tweets/tweets.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TweetsController } from './tweets.controller';
+import { TweetsService } from './tweets.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { TweetDto } from './dto/tweets.dto';
 
 describe('TweetsController', () => {
   let controller: TweetsController;
@@ -7,6 +10,13 @@ describe('TweetsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TweetsController],
+      providers: [
+        TweetsService,
+        {
+          provide: getModelToken('TwitterTweet'),
+          useValue: TweetDto,
+        },
+      ],
     }).compile();
 
     controller = module.get<TweetsController>(TweetsController);
