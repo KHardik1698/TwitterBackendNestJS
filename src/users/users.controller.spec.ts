af229d2b259e473d7a5a37421572185809daae8d
KHardik1698 <hardik.kundalia@contentstack.com>
2021-03-24 17:56:25 +0530

    Imported JwtModule & AuthenticationService
    Added JwtModule in Imports, AuthenticationService in Providers

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -3,15 +3,24 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { getModelToken } from '@nestjs/mongoose';
 import { UserDto } from './dto/users.dto';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthenticationService } from '../authentication/authentication.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        JwtModule.register({
+          secret: process.env.SECRET,
+          signOptions: { expiresIn: '60s' },
+        }),
+      ],
       controllers: [UsersController],
       providers: [
         UsersService,
+        AuthenticationService,
         {
           provide: getModelToken('TwitterUser'),
           useValue: UserDto,
